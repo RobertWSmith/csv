d0a33b28a4ee226f590bbc90531a351b02c279d0
Robert W. Smith <robert.smith@pncbank.com>
2018-07-19 21:01:22 -0400

    fixed log level cmake build option

--- cmake/BuildType.cmake
+++ cmake/BuildType.cmake
@@ -1,13 +1,9 @@
-# set a default build type if not specified
-
-set(default_build_type "Release" CACHE STRING "Default value for CMAKE_BUILD_TYPE if not defined" FORCE)
-if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
-  set(default_build_type "Debug" CACHE STRING "Default value for CMAKE_BUILD_TYPE if not defined" FORCE)
-endif()
-
-if(NOT CMAKE_CONFIGURATION_TYPES)
-  message(STATUS "Setting build type to '${default_build_type}' as none was specified.")
-  set(CMAKE_BUILD_TYPE "${default_build_type}" CACHE STRING "Specifies the build type on single-configuration generators." FORCE)
-  # Set the possible values of build type for cmake-gui
+if(NOT CMAKE_BUILD_TYPE AND NOT CMAKE_CONFIGURATION_TYPES)
   set_property(CACHE CMAKE_BUILD_TYPE PROPERTY STRINGS "Debug" "Release" "MinSizeRel" "RelWithDebInfo")
+  if(EXISTS "${CMAKE_SOURCE_DIR}/.git")
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY VALUE Debug)
+  else()
+    set_property(CACHE CMAKE_BUILD_TYPE PROPERTY VALUE Release)
+  endif()
+  message(STATUS "Setting build type to '${CMAKE_BUILD_TYPE}' as none was specified.")
 endif()
